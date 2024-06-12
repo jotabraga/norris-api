beb583fab202b2e3dbdf15119728b18e0f6806dc
jotabraga <jpmbraga.feg@gmail.com>
2024-06-12 20:38:26 -0300

    feat: add jokes module controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { JokesModule } from './modules/jokes/jokes.module';
+import { JokesModule } from './jokes/jokes.module';
 
 @Module({
   imports: [JokesModule],
