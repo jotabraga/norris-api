63118312ff0791674d14ea4337615d6c17b55956
jotabraga <jpmbraga.feg@gmail.com>
2024-06-15 22:25:16 -0300

    feat: add chuck norris api provider to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JokesModule } from './jokes/jokes.module';
 import { ConfigModule } from '@nestjs/config';
+import { ChuckNorrisModule } from '@/chuck-norris/chuck-norris.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), JokesModule],
+  imports: [ConfigModule.forRoot(), JokesModule, ChuckNorrisModule],
   controllers: [AppController],
   providers: [AppService],
 })
